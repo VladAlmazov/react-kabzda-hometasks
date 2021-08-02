16b0a6eb8b4f14dabb3b7990608b3ec63ffb1995
VladAlmazov <vladosa113@gmail.com>
2021-08-02 21:56:58 +0300

    Add UseEffect

---
+++ src/stories/UseState.stories.tsx
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+
+export default {
+    title: 'useState demo'
+}
+
+function generateData () {
+    // difficult counting
+    console.log('generateData')
+    return 123749146781;
+}
+
+export const Example1 = () => {
+    console.log('Example1');
+    //const initValue = useMemo(generateData, [])
+    const [counter, setCounter] = useState(generateData);
+
+    const changer = (state: number) => state + 1;
+
+    return <>
+        <button onClick={() => setCounter(changer)}>+</button>
+        {counter}
+    </>
+}
